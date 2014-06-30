0456d155c7b93c775b2b469b64992d7a9c897903
Alek Sharma <aleksharma12@gmail.com>
2014-06-30 15:42:35 -0700

    Update introduction text

--- temple.py
+++ temple.py
@@ -6,14 +6,14 @@ print "You are a daring British explorer, bored with your stupid life"
 print "and eager to bring glory to the empire.\n"
 
 print "A few months ago, you heard about an expedition to a temple"
-print "hidden deep in Africa and, because you are a poor planner, you"
-print "quickly signed up.\n"
+print "hidden deep in Africa and, because you don't really think things"
+print "through, you quickly signed up.\n"
 
-print "Now, after several narrow encounters with angry animals, sarcastic"
+print "Now, after a harrowing journey involving angry animals, sarcastic"
 print "natives, and dumb terrain issues, you have made it to the temple."
 
-print "All that remains are a series of deadly traps that have been"
-print "the death of several explorers before you.\n"
+print "All that remains are a series of deadly traps that have claimed"
+print "the lives of several other explorers before you.\n"
 
 print "How hard could it be?\n"
 
