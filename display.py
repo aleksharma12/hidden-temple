fbefeaffb1482712d03cde3557bc969ed71239ee
Alek Sharma <aleksharma12@gmail.com>
2014-06-30 15:52:46 -0700

    Move introduction text to separate display module

---
+++ display.py
@@ -0,0 +1,18 @@
+def introduction():
+  print "\nThe Hidden Temple"
+  print "_________________\n"
+
+  print "You are a daring British explorer, bored with your stupid life"
+  print "and eager to bring glory to the empire.\n"
+
+  print "A few months ago, you heard about an expedition to a temple"
+  print "hidden deep in Africa and, because you don't really think things"
+  print "through, you quickly signed up.\n"
+
+  print "Now, after a harrowing journey involving angry animals, sarcastic"
+  print "natives, and dumb terrain issues, you have made it to the temple."
+
+  print "All that remains are a series of deadly traps that have claimed"
+  print "the lives of several other explorers before you.\n"
+
+  print "How hard could it be?\n"
