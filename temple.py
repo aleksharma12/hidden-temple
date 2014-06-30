0a0d4b43646f4a65a33dfe47458a2648d64d08cd
Alek Sharma <aleksharma12@gmail.com>
2014-06-29 18:21:22 -0700

    Add intro text for game

---
+++ temple.py
@@ -0,0 +1,16 @@
+print "The Hidden Temple\n"
+
+print "You are a daring British explorer, bored with your stupid life"
+print "and eager to bring glory to the empire.\n"
+
+print "A few months ago, you heard about an expedition to a temple"
+print "hidden deep in Africa and, because you are a poor planner, you"
+print "quickly signed up.\n"
+
+print "Now, after several narrow encounters with angry animals, sarcastic"
+print "natives, and dumb terrain issues, you have made it to the temple."
+
+print "All that remains are a series of deadly traps that have been"
+print "the death of several explorers before you.\n"
+
+print "How hard could it be?"
