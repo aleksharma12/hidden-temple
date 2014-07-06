4ed72884895966ca966456c77f2669b5dec962aa
Alek Sharma <aleksharma12@gmail.com>
2014-07-05 22:33:01 -0700

    Make level loading private within engine module

--- engine.py
+++ engine.py
@@ -9,11 +9,11 @@ level_dict = {
 
 #PUBLIC
 def run():
-  load_level('foreword')
+  _load_level('foreword')
   interface.press_enter_to_continue()
-  load_level('gates')
+  _load_level('gates')
 
 #PRIVATE
-def load_level(level):
+def _load_level(level):
     display.print_level_intro(level)
     return level_dict[level]
