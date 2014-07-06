bcfe7ae20fac4dffd9646f12aa98844a66e7a3b5
Alek Sharma <aleksharma12@gmail.com>
2014-07-05 22:29:36 -0700

    Extract driver code into engine.run() * temple.py is now a simple runner for player

--- engine.py
+++ engine.py
@@ -1,11 +1,19 @@
 from levels import *
 import display
+import interface
 
 level_dict = {
     'foreword': Foreword(),
     'gates': Gates()
     }
 
+#PUBLIC
+def run():
+  load_level('foreword')
+  interface.press_enter_to_continue()
+  load_level('gates')
+
+#PRIVATE
 def load_level(level):
     display.print_level_intro(level)
     return level_dict[level]
--- temple.py
+++ temple.py
@@ -1,7 +1,2 @@
-import interface
 import engine
-
-engine.load_level('foreword')
-interface.press_enter_to_continue()
-
-engine.load_level('gates')
+engine.run()
