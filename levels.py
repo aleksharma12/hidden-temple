f7038f46e0724b7fa5c6f1300a55a60a80372733
Alek Sharma <aleksharma12@gmail.com>
2014-07-06 21:46:49 -0700

    Add _gates_graphic() skeleton * may need to be moved to display later

--- levels.py
+++ levels.py
@@ -7,13 +7,13 @@ class Gates(object):
         self.gates = [0,0,0,0,0]
 
     def play(self):
-        display.print_graphic(self._convert_to_graphic())
+        display.print_graphic(self._gates_graphic())
         #while game unfinished
             #display gates graphic
             #ask user to pick a lever
             #display gates graphic
 
-    def _convert_to_graphic(self):
+    def _gates_graphic(self):
         graphic = ["[IIIIIIIIIIIII]"]
         # for gate in gates:
         #     if gate == 0:
