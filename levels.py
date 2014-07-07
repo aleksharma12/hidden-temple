c9350cf8d36d99d18ac9256c424b5a59d752b453
Alek Sharma <aleksharma12@gmail.com>
2014-07-06 21:38:07 -0700

    Add gates attribute to capture their states * All gates start closed

--- levels.py
+++ levels.py
@@ -1,6 +1,11 @@
 class Gates(object):
 
+    def __init__(self):
+        #CLOSED = 0, OPEN = 1
+        self.gates = [0,0,0,0,0]
+
     def play(self):
+        pass
         #while game unfinished
             #display gates graphic
             #ask user to pick a lever
