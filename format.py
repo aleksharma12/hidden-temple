6e291e1e51448e1486d52a549232ca1f3c69f5e8
Alek Sharma <aleksharma12@gmail.com>
2014-07-15 14:28:08 -0700

    Add docstring to format.to_txt * update parameter names

--- format.py
+++ format.py
@@ -1,5 +1,6 @@
-def to_txt(file):
-    return file + '.txt'
+def to_txt(file_name):
+    """Adds the .txt extension to a file name"""
+    return file_name + '.txt'
 
 def capitalize(string):
     return string[:1].upper() + string[1:]
